1a6349deec51990941aa4b816415cd1f9ec060ee
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-29 13:18:34 +0200

    Header Component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Home.scss';
+import Header from '../../components/header/Header';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div>
+        <Header />
+        <h1>Home</h1>
+    </div>
   )
 }
 
