4a0c41ad7b103953572b3266a556e481df8f8a71
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-29 13:48:01 +0200

    Layout Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Layout from './components/layout/Layout';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Layout><Home /></Layout>} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './Home.scss';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
 import loginImg from '../../assets/login.svg';
 
 const Home = () => {
   return (
     <div>
-        <Header />
         <section className="container hero">
             <div className="hero-text">
                 <h2>Ultimate MERN Stack Authentication System</h2>
@@ -24,7 +21,6 @@ const Home = () => {
                 <img src={loginImg} alt="Auth" />
             </div>
         </section>
-        <Footer />
     </div>
   )
 }
