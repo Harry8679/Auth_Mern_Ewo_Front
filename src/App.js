bdf30ab9607528046f2ef378153d45af4bb21423
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-29 23:29:59 +0200

    Forgot Password Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/home/Home';
 import Layout from './components/layout/Layout';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import Forgot from './pages/auth/Forgot';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path='/' element={<Layout><Home /></Layout>} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/forgot' element={<Forgot />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/auth/Forgot.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import styles from "./auth.module.scss";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
+import { GoMail } from "react-icons/go";
+
+
+const Forgot = () => {
+  const [email, setEmail] = useState('');
+
+  const handleInputChange = () => {};
+
+  const loginUser = () => {};
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <GoMail size={35} color="#999" />
+          </div>
+          <h2>Forgot Password</h2>
+
+          <form onSubmit={loginUser}>
+            <input type='email' placeholder='Email' required name='email' value={email} onChange={handleInputChange} />
+
+            <button className="--btn --btn-primary --btn-block" type="submit">Get Reset Email</button>
+
+            <span className={styles.links}>
+                <p><Link to='/'>- Home</Link></p>
+                <p><Link to='/register'>- Register</Link></p>
+            </span>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Forgot;
