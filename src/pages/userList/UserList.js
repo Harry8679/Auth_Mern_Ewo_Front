d76522e40ed343803eaf839aa14aa4808c694ded
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-30 10:38:26 +0200

    User Table

--- src/pages/userList/UserList.js
+++ src/pages/userList/UserList.js
@@ -3,6 +3,7 @@ import './UserList.scss';
 import PageMenu from '../../components/pageMenu/PageMenu';
 import UserStats from '../../components/userStats/UserStats';
 import Search from '../../components/search/Search';
+import { FaTrashCan } from "react-icons/fa6";
 
 const UserList = () => {
   return (
@@ -12,9 +13,33 @@ const UserList = () => {
             <UserStats />
 
             <div className="user-list">
-                <div className="--flex-between">
-                    <span><h3>All Users</h3></span>
-                    <span><Search /></span>
+                <div className="table">
+                    <div className="--flex-between">
+                        <span><h3>All Users</h3></span>
+                        <span><Search /></span>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>s/n</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Change Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Harry</td>
+                                <td>harry.maccode@lhlp.com</td>
+                                <td>Admin</td>
+                                <td>ChangeRole</td>
+                                <td><span><FaTrashCan size={20} color='red' /></span></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
